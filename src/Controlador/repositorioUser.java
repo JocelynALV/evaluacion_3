d682cca0b8d178051250ddb5a5fff7712a7af37b
evely <evely@LAPTOP-BBVEJBEG>
2021-11-30 20:30:58 -0300

    Dentro del repositorioUser se crea eliminar y buscar User

--- src/Controlador/repositorioUser.java
+++ src/Controlador/repositorioUser.java
@@ -6,6 +6,7 @@ import Modelo.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 
 /**
@@ -69,4 +70,56 @@ public class repositorioUser {
         }
     }
     
+    public boolean eliminarUser(String username){
+        try {
+            Connection con = conectar.ObtConexion();
+            
+            String query = "DELETE FROM usuario WHERE username = ?";
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setString(1, username);
+            
+            stmt.executeUpdate();
+            stmt.close();
+            con.close();
+            
+            return true;
+            
+        } catch (SQLException e) {
+            
+            System.out.println("Error en SQL al eliminar USER"+e.getMessage());
+            return false;
+
+        }
+    }
+    
+    public Usuario buscarUsuario(int IDusuario){
+        
+        Usuario usuario = new Usuario();
+        
+        try {
+            Connection con = conectar.ObtConexion();
+            
+            String query = "SELECT IDusuario,nombre,apellido,username,password WHERE IDusuario = ?";
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, IDusuario);
+            
+            ResultSet resultado = stmt.executeQuery(); // ResulSet trae un SELECT
+            
+            if (resultado.next()) { //Si existe el dato muestralo, si el SELECT no existe pasa de largo.
+                usuario.setIDusuario(resultado.getShort("IDusuario"));
+                usuario.setNombre(resultado.getString("nombre"));
+                usuario.setApellido(resultado.getString("apellido"));
+                usuario.setUsername(resultado.getString("username"));
+                usuario.setPassword(resultado.getNString("password"));
+            }
+            resultado.close();
+            stmt.close();
+            con.close();
+            
+        } catch (SQLException e) {
+            
+            System.out.println("Error en SQL al Listar USER"+e.getMessage());
+        }
+        return usuario;
+    }
 }
